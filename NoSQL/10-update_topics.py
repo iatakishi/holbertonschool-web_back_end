949e6a675e9c1154611d8d710d4d8773c302c87d
Idrak <iatakishi@gmail.com>
2026-04-01 21:51:39 +0400

    10 added

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,9 +3,9 @@
 
 
 def update_topics(mongo_collection, name, topics):
-    """ updating the topics """
+    """Change all topics of a school document based on the name."""
     return mongo_collection.update(
         {"name": name},
-        {"$set": {"topics": topics }},
-        {multi: true}
+        {"$set": {"topics": topics}},
+        multi=True
     )
